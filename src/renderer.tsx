80820c6f85b9eee90ebbcf44e60b6e69704b4244
Abid Famasya <abid.famasya@gmail.com>
2025-05-04 14:15:59 +0700

    fix: style

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -1,13 +1,28 @@
+import { css, Style } from 'hono/css'
 import { jsxRenderer } from 'hono/jsx-renderer'
 
 export const renderer = jsxRenderer(({ children }) => {
   return (
     <html>
       <head>
-        <link
-          href={import.meta.env.PROD ? `/assets/style.css` : `/src/style.css`}
-          rel="stylesheet"
-        />
+        <Style>{css`
+          body {
+            font-family: Arial, Helvetica, sans-serif;
+          }
+
+          h4 {
+            margin: 0px;
+          }
+
+          body ul {
+            padding-left: 20px;
+            margin: 5px;
+          }
+
+          body a {
+            text-decoration: none;
+          }
+        `}</Style>
         <title>News API ID - API Berita Bahasa Indonesia</title>
       </head>
       <body>{children}</body>
